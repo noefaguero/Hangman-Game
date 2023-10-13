d130cf52ba6a298a5985116d8841ded1ca651fd7
Noé Fernández Agüero <noefernandezaguero@MacBook-Pro-de-Noe.local>
2023-10-13 19:43:12 +0200

    First commit: HTML and CSS done

---
+++ js/script.js
@@ -0,0 +1,91 @@
+// Words array
+let posiblesPalabras = [
+  ["amarillo", "Un color"],
+  ["atlantico", "Un océano"],
+  ["ordenador", "Una gran herramienta  ;)"],
+  ["laurel", "Un árbol"],
+  ["plaza", "Espacio público"],
+  ["avenida", "Espacio público"],
+  ["calle", "Espacio público"],
+  ["desarrolador", "Una profesión"],
+  ["rueda", "Gran invento"],
+  ["cereza", "Una fruta"],
+  ["petanca", "Un juego"],
+  ["pintor", "Una profesión"],
+  ["naranjo", "Un árbol"],
+  ["higuera", "Un árbol"],
+  ["everest", "Un monte"],
+  ["relampago", "Antecede al trueno"],
+  ["jirafa", "Un animal"],
+  ["nogal", "Un árbol"],
+  ["tigre", "Un animal"],
+  ["elefante", "Un animal"],
+  ["mosquito", "Un insecto"],
+  ["caballo", "Un animal"],
+  ["rinoceronte", "Un animal"],
+  ["portugal", "Un país"],
+  ["españa", "Un país"],
+  ["noruega", "Un país"],
+  ["italia", "Un país"],
+  ["rusia", "Un país"],
+  ["uruguay", "Un país"],
+  ["ilustracion", "Representación gráfica"],
+  ["tarta", "De la pastelería"],
+  ["pepito", "De la pastelería"],
+  ["excursion", "Actividad en la naturaleza"],
+  ["empanadilla", "De la panadería"],
+  ["pastel", "De la pastelería"],
+  ["colegio", "Lugar para estudiar"],
+  ["carrera", "Competición"],
+  ["mermelada", "Confitura"],
+];
+
+let abecedario = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "Ñ",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+
+// to show the status of successes
+let palabra = document.getElementById("palabra");
+
+
+// button to start the game again
+let inicio = document.getElementById("inicio");
+
+let botonera = document.getElementById("botonera");
+let intentos = document.getElementById("intentos");
+let imagen = document.getElementById("imagen");
+let pista = document.getElementById("pista");
+
+//select the secret word and the clue
+let secreta = posiblesPalabras[Math.floor(Math.random()*posiblesPalabras.length)]
+let palabrasecreta = secreta[0] 
+let palabraoculta = ""
+
+
